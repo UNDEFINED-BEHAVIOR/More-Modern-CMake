e651ad41bb854dfd8ab10bcfe908c6e1cf32d34e
Bantukul Olarn <bo.bantukulolarn@gmail.com>
2019-06-18 03:40:14 +0900

    Remove older samples

--- example-2.8.10/library/include/Math.h
+++
@@ -1,6 +0,0 @@
-#include "boost/outcome.hpp"
-
-namespace outcome = OUTCOME_V2_NAMESPACE;
-
-outcome::result<int> convert(const std::string& str) noexcept;
-
--- example-3.10/library/src/BasicMath.cpp
+++
@@ -1,6 +0,0 @@
-#include "Math.h"
-
-outcome::result<int> convert(const std::string& str) noexcept
-{
-        return outcome::result<int>{ 0 };
-}
